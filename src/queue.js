99b26ccfe3a4dc43befe09ed2192c413821e7d2a
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-04-17 12:26:44 +0300

    feat: implement queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -16,18 +16,28 @@ const { NotImplementedError } = require('../extensions/index.js');
 class Queue {
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value);
+
+    if (!this.queue) {
+      this.queue = node;
+    } else {
+      let current = this.queue;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.queue;
+    this.queue = this.queue.next;
+
+    return current.value;
   }
 }
 
